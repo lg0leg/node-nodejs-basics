d5047edd830068dcc89695de647bc4e64e099dcf
lg0leg <lg.oleg1067@gmail.com>
2025-04-26 15:03:27 +0300

    feat: add "calcHash.js" solution

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { createHash } from 'crypto';
+import { createReadStream } from 'fs';
+import { pipeline } from 'stream/promises';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const filePath = `${__dirname}/files/fileToCalculateHashFor.txt`;
+
+  const hash = createHash('sha256');
+  const readStream = createReadStream(filePath);
+
+  try {
+    await pipeline(readStream, hash);
+
+    console.log(hash.digest('hex'));
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await calculateHash();
