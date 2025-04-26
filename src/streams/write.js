51c95b0c6fd13e49926010a6cc6b741097371e4e
lg0leg <lg.oleg1067@gmail.com>
2025-04-26 17:58:40 +0300

    feat: add "write.js" solution

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,20 @@
+import { join } from 'path';
+import { createWriteStream } from 'fs';
+import { pipeline } from 'stream/promises';
+import { stdin } from 'process';
+
 const write = async () => {
-    // Write your code here 
+  const __dirname = import.meta.dirname;
+  const filePath = join(__dirname, 'files', 'fileToWrite.txt');
+  const writeStream = createWriteStream(filePath, 'utf8');
+
+  try {
+    await pipeline(stdin, writeStream);
+  } catch (err) {
+    throw new Error(err);
+  }
 };
 
 await write();
+
+//use "ctrl + C" to to stop writing
