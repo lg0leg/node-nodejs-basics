110c2f0eb9ca01f3a2510db58e56593125ae2d97
lg0leg <lg.oleg1067@gmail.com>
2025-04-26 17:25:48 +0300

    feat: add "read.js" solution

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,20 @@
+import { join } from 'path';
+import { createReadStream } from 'fs';
+import { pipeline } from 'stream/promises';
+import { stdout } from 'process';
+
 const read = async () => {
-    // Write your code here 
+  const __dirname = import.meta.dirname;
+  const filePath = join(__dirname, 'files', 'fileToRead.txt');
+  const readStream = createReadStream(filePath, 'utf8');
+
+  try {
+    await pipeline(readStream, stdout);
+  } catch (err) {
+    throw new Error(err);
+  }
 };
 
 await read();
+
+//please use "node src/streams/read.js" instead of script if the console is empty
