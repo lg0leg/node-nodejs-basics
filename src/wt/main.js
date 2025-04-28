cca8a9fe1389cee9d492c4ef7146946158845075
lg0leg <lg.oleg1067@gmail.com>
2025-04-28 18:53:51 +0300

    feat: add "worker.js" solution

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,31 @@
+import { Worker } from 'worker_threads';
+import { cpus } from 'os';
+import { join } from 'path';
+
 const performCalculations = async () => {
-    // Write your code here
+  const __dirname = import.meta.dirname;
+
+  const coreCount = cpus().length;
+  const promises = [];
+
+  for (let i = 0; i < coreCount; i++) {
+    const prom = new Promise((resolve, reject) => {
+      const worker = new Worker(join(__dirname, 'worker.js'), {
+        workerData: 10 + i,
+      });
+      worker.on('message', resolve);
+      worker.on('error', reject);
+      worker.on('exit', (code) => {
+        if (code !== 0) reject(new Error(`Worker stopped with exit code ${code}`));
+      });
+    });
+
+    promises.push(prom);
+  }
+
+  const results = await Promise.all(promises);
+
+  console.log('Results:', results);
 };
 
 await performCalculations();
